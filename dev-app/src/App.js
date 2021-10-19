31005cdbf9c08a3a22b27d0165ed9776ce0fbd03
ukamila <ukamila@bu.edu>
2021-10-19 17:53:36 -0400

    "added eslint"

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { Homepage } from './pages/homepage/homepage';
+import {Homepage} from './pages/homepage';
 
 function App() {
   return (
     <div className="App">
       <p> BPDA Main Streets </p>
       <Homepage />
-      <header className="App-header">
-      </header>
     </div>
   );
 }
---
+++ dev-app/src/pages/homepage/index.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './index.tsx.css';
+
+interface Props {
+    data?: string
+}
+
+const data = [
+  {
+    name: 'Massachusetts Behavioral Health Partnership',
+    type: 'Health Care and Social Assistance',
+  },
+  {
+    name: 'United States Monorail Corp',
+    type: 'Manufacturing',
+  },
+  {
+    name: 'Massachusetts Professional Licensing',
+    type: 'Public Administration',
+  },
+  {
+    name: 'Mass Commonwealth Insurance',
+    type: 'Public Administration',
+  },
+  {
+    name: 'E G Leon & Co',
+    type: 'Construction',
+  },
+  {
+    name: 'Elephant Walk South End',
+    type: 'Accommodation and Food Services',
+  },
+  {
+    name: 'Zeybekoglu & Associates',
+    type: 'Professional, Scientific, and Technical Services',
+  },
+  {
+    name: 'Ming\'s Supermarket',
+    type: 'Retail Trade',
+  },
+  {
+    name: 'C Mart of Herald St',
+    type: 'Retail Trade',
+  },
+  {
+    name: 'Connelly Partners',
+    type: 'Professional, Scientific, and Technical Services',
+  },
+  {
+    name: 'Asian American Civic Association',
+    type: 'Manufacturing',
+  },
+  {
+    name: 'Flour Bakery',
+    type: 'Manufacturing',
+  },
+];
+
+class Homepage extends React.Component<Props, {}> {
+  render() {
+    return (
+      <div>
+        <span className="span">Homepage</span>
+        {data.map((item) =>
+          <div className="list" key="item.id">
+            <div className="name">{item.name}</div>
+            <div className="type">{item.type}</div>
+          </div>,
+        )}
+      </div>
+    );
+  }
+}
+
+export {Homepage};
