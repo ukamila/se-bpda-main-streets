e9c5f19caad7ac6247c4f289ab56d5cc8d7c6500
ukamila <ukamila@bu.edu>
2021-11-07 23:43:52 -0500

    removed google map api

--- dev-app/src/App.js
+++ dev-app/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Homepage} from './pages/homepage';
-import {Map} from './components/Map';
+
 
 function App() {
-  const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
-  };
-
   return (
     <div className="App">
       <p> BPDA Main Streets</p>
-      <Map location={location} zoomLevel={17} />
       <Homepage />
     </div>
   );
--- dev-app/src/components/Map/index.tsx
+++ dev-app/src/components/Map/index.tsx
@@ -1,8 +1,6 @@
 import React, {FC} from 'react';
 import {useEffect} from 'react';
-import GoogleMapReact from 'google-map-react';
 import './index.tsx.css';
-import {LocationPin} from '../LocationPin';
 
 interface Props {
   location: any;
@@ -23,17 +21,6 @@ const Map: FC<Props> = ({location, zoomLevel}) => {
     <div className="map">
       <h2 className="map-h2">Come Visit Us At Our Campus</h2>
       <div className="google-map">
-        <GoogleMapReact
-          bootstrapURLKeys={{key: ''}}
-          defaultCenter={location}
-          defaultZoom={zoomLevel}
-        >
-          <LocationPin
-            lat={location.lat}
-            lng={location.lng}
-            text={location.address}
-          />
-        </GoogleMapReact>
       </div>
     </div>
   );
